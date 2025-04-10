8ada9fa3e80bca8347c6d95f1a4aea201043b287
Ch-Tayyaba <tayyabaafzal957@gmail.com>
2025-04-10 13:40:17 +0500

    Quizes

---
+++ Quiz/Smoothie(2022cs134)/Smoothie(2022cs134)/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Ques2
+{
+	internal class Program
+	{
+		static List<ingredients> items = new List<ingredients>();
+		static void Main(string[] args)
+		{
+			ingredients ingredient1 = new ingredients("Strawberries", 1.50);
+			items.Add(ingredient1);
+			ingredients ingredient2 = new ingredients("Banana", 0.50);
+			items.Add(ingredient2);
+			ingredients ingredient3 = new ingredients("Mango", 2.50);
+			items.Add(ingredient3);
+			ingredients ingredient4 = new ingredients("Blueberries", 1.00);
+			items.Add(ingredient4);
+			ingredients ingredient5 = new ingredients("Raspberries", 1.00);
+			items.Add(ingredient5);
+			ingredients ingredient6 = new ingredients("Apple", 1.75);
+			items.Add(ingredient6);
+			ingredients ingredient7 = new ingredients("Pineapple", 3.50);
+			items.Add(ingredient7);
+
+			List<string> smoothies = GetSmoothieFromInput();
+			Smoothie s = new Smoothie(smoothies);
+			outputResult(s);
+		}
+
+		public static void outputResult(Smoothie s)
+		{
+
+			Console.WriteLine(Math.Round(s.GetCost(), 2));
+			Console.WriteLine(Math.Round(s.GetPrice(), 2));
+			Console.WriteLine(s.GetName());
+		}
+		public static List<string> GetSmoothieFromInput()
+		{
+			int n = int.Parse(Console.ReadLine());
+			List<string> ingredients = new List<string>();
+			for (int i = 0; i < n; ++i)
+			{
+				string _params = Console.ReadLine();
+				ingredients.Add(_params);
+			}
+			return ingredients;
+		}
+
+		public class Smoothie
+		{
+			public List<string> Ingredients = new List<string>();
+			public Smoothie(List<string> Ingredients)
+			{
+				// Write Code Here
+				this.Ingredients = Ingredients;
+				
+			}
+
+			public double GetCost()
+			{
+				// Write Code Here
+				double cost = 0;
+				for(int idx = 0; idx < Ingredients.Count; idx++)
+                {
+					if(Ingredients[idx] == "Stawberries")
+                    {
+						cost = cost + 1.50F;
+                    }
+					if (Ingredients[idx] == "Banana")
+					{
+						cost = cost + 0.50F;
+					}
+					if (Ingredients[idx] == "Mango")
+					{
+						cost = cost + 2.50F;
+					}
+					if (Ingredients[idx] == "Blueberries")
+					{
+						cost = cost + 1.00F;
+					}
+					if (Ingredients[idx] == "Raspberries")
+					{
+						cost = cost + 1.00F;
+					}
+					if (Ingredients[idx] == "Apple")
+					{
+						cost = cost + 1.75F;
+					}
+					if (Ingredients[idx] == "Pineapple")
+					{
+						cost = cost + 3.50F;
+					}
+				}
+				return cost;
+			}
+			public double GetPrice()
+			{
+				// Write Code Here
+			}
+			public string GetName()
+			{
+
+				// Write Code Here
+			}
+
+
+		}
+		public class ingredients
+		{
+			public string name;
+			public double price;
+
+			public ingredients(string name, double price)
+			{
+				// Write Code Here	
+				this.name = name;
+				this.price = price;
+			}
+		}
+
+
+	}
+}
---
+++ Quiz/SweetestIcecream(2022cs134)/SweetestIcecream(2022cs134)/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IceCreams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IceCream> ices = GetIceCreamsFromInput();
+            Console.WriteLine(SweetestIceCream(ices));
+            Console.ReadKey();
+        }
+
+        public static List<IceCream> GetIceCreamsFromInput()
+        {
+            List<IceCream> iceCreams = new List<IceCream>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; ++i)
+            {
+                string[] _params = Console.ReadLine().Split(' ');
+                IceCream _iceCream = new IceCream(_params[0], int.Parse(_params[1]));
+                iceCreams.Add(_iceCream);
+            }
+
+            return iceCreams;
+        }
+
+        public static int SweetestIceCream(List<IceCream> icecream)
+        {
+            // Write your Code Here
+            int sweetest = -1;
+            for (int idx = 0; idx < icecream.Count; idx++)
+            {
+                int numSprinkles;
+                if (icecream[idx].flavour == "Vanilla" || icecream[idx].flavour == "ChocolateChip")
+                {
+                    numSprinkles = icecream[idx].sprinkles + 5;
+                }
+                else if (icecream[idx].flavour == "Stawberry" || icecream[idx].flavour == "Chocolate")
+                {
+                    numSprinkles = icecream[idx].sprinkles + 10;
+                }
+                else
+                {
+                    numSprinkles = icecream[idx].sprinkles;
+                }
+                if (sweetest < numSprinkles)
+                {
+                    sweetest = numSprinkles;
+                }
+            }
+            return sweetest;
+
+        }
+    }
+
+    public class IceCream
+    {
+        // Write your Class Here
+        public string flavour;
+        public int sprinkles;
+        public IceCream(string flavour, int sprinkles)
+        {
+            this.flavour = flavour;
+            this.sprinkles = sprinkles;
+        }
+    }
+}
+
+
